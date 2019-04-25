24dd894b150d03886838191fcdeff13d31195bc8
HBT-hugo <hbt.hugo@gmail.com>
2019-04-25 16:16:17 -0300

    traduções, rotas, migrates e views{comments}

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,47 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "hbt", password: "secreto", except: [:index, :show]
+
+	def index
+    	@articles = Article.all
+  	end
+
 	def show
     	@article = Article.find(params[:id])
   	end
 
 	def new
+		@article = Article.new
+	end
+
+	def edit
+  		@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(article_params)
  
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'Novo'
+		end
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+ 
+  		if @article.update(article_params)
+    		redirect_to @article
+  		else
+    		render 'Editar'
+  		end
+	end
+
+	def destroy
+  		@article = Article.find(params[:id])
+  		@article.destroy
+ 
+ 		redirect_to articles_path
 	end
  
 	private
